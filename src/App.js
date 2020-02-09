ab4b0dd45a8e6aa5ba14d1cb669403139ea41e90
ctoogood <calumtoogood@gmail.com>
2020-02-09 14:41:24 +0100

    started compiling home page and added router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import RecipeList from './components/RecipeList';
+import RecipeDetail from './components/RecipeDetail';
+import Home from './components/Home';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+const App = () => (
+  <Router>
+    <Home />
+    <main>
+      <Switch>
+        <Route exact path="/" />
+        <Route path="/query/:query" component={RecipeList} />
+        <Route path="/:id" component={RecipeDetail} />
+      </Switch>
+    </main>
+  </Router>
+);
 
 export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import bgImage from '../images/spoonOfCheese.jpg';
+
+
+const Main = styled.main`
+  height:100vh;
+  width:100%;
+  margin:0;
+  overflow:hidden;
+  padding-top:5rem;
+  background-image:url(${bgImage});
+  background-size:cover;
+  background-position:center;
+  overflow:hidden;
+  border-bottom: 2rem solid #FF5D5D;
+
+  h1 {
+    font:3rem Third Storey;
+    text-align:center;
+    margin:0;
+    color:#F83737;
+    margin:1rem;
+
+    @media only screen and (min-width:600px) {
+      font-size:5rem;
+      margin:0;
+      padding:4rem;
+      line-height:1.5;
+    }
+    
+    @media only screen and (min-width:900px) {
+      font-size:7rem
+    }
+  }
+
+  .home {
+    text-decoration:none;
+  }
+  `;
+
+const Home = () => (
+  <Main>
+    <h1>A Pinch Of Salt...</h1>
+  </Main>
+);
+
+export default Home;
