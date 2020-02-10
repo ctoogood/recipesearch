42fad8952c714bda3e184fd3d3b84154b0f1670a
ctoogood <calumtoogood@gmail.com>
2020-02-10 17:43:20 +0100

    Added and styled recipes list

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,11 +1,81 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
-const Recipe = () => {
-    return (
-        <div>
-            
+const RecipeDisplay = styled.section`
+    margin:auto;
+    border-radius:10px;
+    margin-bottom:1rem;
+    @media only screen and (min-width:480px) {
+      margin-bottom:0;
+    }
+
+    .recipe__link {
+      text-decoration:none;
+      
+      &:hover img {
+        filter:brightness(1);
+      }
+
+      &:hover h1 {
+        background-color:#FF5D5D;
+        border-radius:5px;
+      }
+    }
+
+    h1 {
+        font-family: Third Storey;
+        font-size:1.5rem;
+        color: white;
+        position:absolute;
+        top:40%;
+        left:50%;
+        transform:translate(-50%,-50%);
+        z-index:2;
+        text-align:center;
+        line-height:1.5;
+        transition:all 0.3s ease-in-out;
+        padding:0.5rem;
+
+    }
+
+    .recipe__image {
+      position:relative;
+    }
+      
+    img {
+      width:100%;
+      object-fit:cover;
+      filter:brightness(0.7);
+      box-shadow:0px 3px 3px rgba(0,0,0,0.2);
+      border-radius:5px;
+      transition:all 0.3s ease-in-out;
+
+    }
+`;
+
+
+const Recipe = ({ recipe }) => {
+  return (
+    <RecipeDisplay>
+      <Link to={`/${recipe.id}`}>
+        <div className="recipe__image">
+          <h1>{recipe.label}</h1>
+          <img src={recipe.image} alt={recipe.label} />
         </div>
-    );
+      </Link>
+    </RecipeDisplay>
+  );
 };
 
 export default Recipe;
+
+Recipe.propTypes = {
+  recipe: PropTypes.shape({
+    label: PropTypes.string.isRequired,
+    image: PropTypes.string,
+    id: PropTypes.number.isRequired,
+    spoonacularSourceUrl: PropTypes.string,
+  }).isRequired,
+};
--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -1,15 +1,61 @@
+/* eslint-disable no-nested-ternary */
 import React, { useContext } from 'react';
+import styled from 'styled-components'
 import { RecipeContext } from '../context/index';
 import Header from './Header';
+import Recipe from './Recipe';
+
+const List = styled.section`
+    max-width: 1000px;
+    margin:auto;
+    margin-bottom:1rem;
+    margin-top:1rem;
+
+    
+    @media only screen and (min-width:480px) {
+      display:grid;
+      grid-template-columns: 1fr 1fr;
+      grid-gap:2rem;
+    }
+  
+    @media only screen and (min-width:800px) {
+      grid-template-columns: 1fr 1fr 1fr;
+      margin:auto;
+      margin-top:2rem;
+      margin-bottom:2rem;
+  
+  
+    }
+
+    .loading {
+      text-align:center;
+      font-family: Third Storey;
+      color: #F83737;
+      height:100vh;
+    }
+`;
+
+const Loading = styled.h1`
+    text-align:center;
+    font-family: Third Storey;
+    color: #F83737;
+    height:100vh;
+`;
 
 const RecipeList = () => {
   const appContext = useContext(RecipeContext);
   const { recipes, loading } = appContext;
   return (
-    <div>
+    <>
       <Header />
+      {loading ? <Loading>Loading...</Loading>
+        : recipes.length > 0 ? (
+          <List>
+            {recipes.map((recipe) => <Recipe recipe={recipe.recipe} key='' />)}
+          </List>
+        ) : <Loading className="loading">No Results, Try Again..</Loading> }
 
-    </div>
+    </>
   );
 };
 
